2d0e219a5aa63a08767c4651522e51558c314659
nanjizal <nitsuj@nanjizal.net>
2017-06-23 16:56:09 +0100

    Changed to render all experiments and use colours.

--- src/Main.hx
+++ src/Main.hx
@@ -3,89 +3,60 @@ import luxe.Input;
 import luxe.Vector;
 import lsystem.*;
 import target.LuxeLNode;
+import phoenix.geometry.Geometry;
 import luxe.Color;
+import drawings.*;
+
 //  All axioms and rules used as examples below come from the wikipedia page for L-Systems 
 //  ( http://en.wikipedia.org/wiki/L-system ).
+//  plantD, plantE, plantF from 1990 book Algorithmic Beauty of Plants.
+//  another source http://www.kevs3d.co.uk/dev/lsystems/
 class Main extends luxe.Game {
+    var green: Color;
+    var blue: Color;
+    var red: Color;
+    var yellow: Color;
+    var darkRed: Color;
+    var white: Color;
+    var grey: Color;
+    var currentColor: Color;
+    var graphics: Array<Geometry>;
+    var drawing: Drawing;
+    var drawings: Array<Drawing>;
+    var colors: Array<Color>;
     override function ready() {
-        //fractalPlant();
-        //dragonCurve();
-        sierpinskiTriangle();
-    } //ready
-    function fractalPlant() : Void {
-        var options = { axiom : "A" };
-        var lsystem = new LSystem(options);
-        lsystem.setRule( "A", "B-[[A]+A]+B[+BA]-A" );
-        lsystem.setRule( "B", "BB" );
-        lsystem.iterate(4);
-        var node = new LuxeLNode( centre(), 270.0 );
-        var stack = new StateStack( cast node );
-        var axiom = lsystem.options.axiom;
-        var l = axiom.length;
-        for( i in 0...l ){
-            switch( axiom.charAt(i) ){
-                case '[':
-                    stack.pushSame();
-                case ']':
-                    stack.pop();
-                case '+':
-                    stack.rotate(25.0);
-                case '-':
-                    stack.rotate(-25.0);
-                case 'A','B':
-                    luxeLine( cast stack.forwardDraw(4) );
-            }
-        }
+        green = new Color().rgb(0x00ff00);
+        blue = new Color().rgb(0x0000ff);
+        red = new Color().rgb(0xff0000);
+        yellow = new Color().rgb( 0xFFFF00 );
+        white = new Color().rgb( 0xFFFFFF );
+        grey = new Color().rgb( 0xcccccc );
+        darkRed = new Color( 0.5, 0.2, 0.2, 1 );
+        currentColor = red;
+        graphics = new Array<phoenix.geometry.Geometry>();
+        colors =   [    red, green, blue, red, green, green, red, green, yellow, red, grey ];
+        drawings = [    new PenoTriangle( position( 700, 600), line )
+                    ,   new FunnyTriangle( position( 100, 600 ), line )
+                    ,   new Tiles( centre(), line )
+                    ,   new Seaweed( position( 550, 600 ), line, setColor.bind( 3 ) )
+                    ,   new PondWeed( position( 750, 600 ), line, setColor.bind( 4 ) )
+                    ,   new SierpinskiSnowFlake( position( 500, 200 ), line )
+                    ,   new Plant( position( 90, 600 ), line )
+                    ,   new PlantE( position( 800, 300 ), line )
+                    ,   new PlantD( position( 400, 400 ), line )
+                    ,   new DragonCurve( topLeft(), line )
+                    ,   new SierpinskiTriangle( bottomRight(), line )
+        ];
     }
-    function dragonCurve() : Void {
-        var options = { axiom : "FX" };
-        var lsystem = new LSystem( options );
-        lsystem.setRule( "X", "X+YF" );
-        lsystem.setRule( "Y", "FX-Y" );
-        lsystem.iterate( 10 );
-        var node = new LuxeLNode( centre(), 270.0 );
-        var stack = new StateStack( cast node );
-        var axiom = lsystem.options.axiom;
-        var l = axiom.length;
-        for( i in 0...l ){
-            switch( axiom.charAt(i) ){
-                case "[":
-                    stack.pushSame();
-                case "]":
-                    stack.pop();
-                case "+":
-                    stack.rotate(90.0);
-                case "-":
-                    stack.rotate(-90.0);
-                case "F":
-                    luxeLine( cast stack.forwardDraw(12) );
-            }
-        }
+    inline public function setColor( count: Int, col: Color ){
+        colors[ count ] = col;
     }
-    function sierpinskiTriangle() : Void {
-        var options = { axiom : "A" };
-        var lsystem = new LSystem(options);
-        lsystem.setRule("A", "B-A-B");
-        lsystem.setRule("B", "A+B+A");
-        lsystem.iterate(8);
-        var node = new LuxeLNode( bottomRight(), 180.0 );
-        var stack = new StateStack( cast node );
-        var axiom = lsystem.options.axiom;
-        var l = axiom.length;
-        for( i in 0...l ){
-            switch( axiom.charAt(i) ){
-                case "[":
-                    stack.pushSame();
-                case "]":
-                    stack.pop();
-                case "+":
-                    stack.rotate(60.0);
-                case "-":
-                    stack.rotate(-60.0);
-                case 'A','B':
-                    luxeLine( cast stack.forwardDraw(2) );
-            }
-        }
+    public function clear():Void {
+        var geom;
+        while ((geom = graphics.pop()) != null) geom.drop();
+    }
+    inline function position( x: Float, y: Float ): Vector {
+        return new Vector( x, y );
     }
     inline function centre():Vector {
         return new Vector( Luxe.screen.w / 2, Luxe.screen.h / 2 );
@@ -93,17 +64,41 @@ class Main extends luxe.Game {
     inline function bottomRight():Vector{
         return new Vector( Luxe.screen.w - 50, Luxe.screen.h - 50 );
     }
-    inline function luxeLine( line ){
+    inline function topLeft():Vector{
+        return new Vector( 80, 170 );
+    }
+    inline function line( line ){
         var start: LuxeLNode = line.start;
         var end: LuxeLNode = line.end;
-        Luxe.draw.line({
+        var geom = Luxe.draw.line({
             p0 : start.pos.clone(),
             p1 : end.pos.clone(),
-            color : new Color( 0.5, 0.2, 0.2, 1 )
+            color : currentColor
         });
+        graphics[ graphics.length ] = geom;
     }
     override function onkeyup( e: KeyEvent ){
         if( e.keycode == Key.escape ) Luxe.shutdown();
     }
-    override function update( dt: Float ){}
+    var time: Int = 0;
+    var speed: Int = 100;
+    override function update( dt: Float ){
+        if( time == 1000 ) {
+            speed = 10;
+            clear();
+        }
+        for( i in 0...speed ){
+            var j = 0;
+            for( drawing in drawings ){
+                if( time > 1000 || ( j == 3 || j == 4 ) ){
+                    currentColor = colors[ j ];
+                    if( drawing.hasNext() ){
+                        drawing.draw();
+                    } else {}
+                }
+                j++;
+            }
+        }
+        time++;
+    }
 }
