a254de26a9c53edf7a54cbef1836db469852b30d
nanjizal <nitsuj@nanjizal.net>
2022-11-21 00:11:52 +0000

    tweak code formatting

--- src/lsystemx/nodes/LNode.hx
+++ src/lsystemx/nodes/LNode.hx
@@ -5,10 +5,10 @@ import lsystemx.Mat1x2;
 **/
 //@:structInit
 class LNode {
-    public var pos : Mat1x2;
-    public var angle : Float;
+    public var pos:   Mat1x2;
+    public var angle: Float;
     public function new( pos_: Mat1x2, angle_: Float ){
-        pos = pos_;
+        pos   = pos_;
         angle = angle_;
     }
     @:keep
@@ -16,20 +16,27 @@ class LNode {
     function transform( distance: Float ): LNode {
         var deg = Deg2Rad( angle );
         return new LNode( 
-                new Mat1x2({ x: pos.x + Math.cos( deg ) * distance, y: pos.y + Math.sin( deg ) * distance } ),
-                angle );
+            new Mat1x2({ 
+                    x: pos.x + Math.cos( deg ) * distance
+                ,   y: pos.y + Math.sin( deg ) * distance 
+                },  angle );
     }
     @:keep
     public inline
     function clone(): LNode {
-        return new LNode( {x: pos.x, y: pos.y }, angle );
+        return new LNode( 
+            new Mat1x2({ 
+                    x: pos.x
+                ,   y: pos.y
+                },  angle );
     }
     @:keep
     public inline
     function addAngle( q: Float ): Void {
         angle += q;
     }
-    inline function Deg2Rad(degrees : Float) : Float{
-        return degrees * (Math.PI / 180.0);
+    inline
+    function Deg2Rad(degrees : Float) : Float{
+        return degrees * ( Math.PI / 180.0 );
     }
 }
